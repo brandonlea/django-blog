10f774b4f0e5e6d830c4412f812a9be8ca4bfef8
Brandon-Lea Price <brandonlea810@gmail.com>
2025-09-19 20:45:04 +0100

    Added more and removed pycache.

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,30 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on", "author"]
+    
+    def __str__(self):
+        return f"The title of the this post is {self.title}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
+    

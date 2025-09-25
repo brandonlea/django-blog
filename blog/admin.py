922f16790f86ed620ab88f4db299f30cb77bb9c3
Brandon-Lea Price <brandonlea810@gmail.com>
2025-09-25 20:43:51 +0100

    Added about me section.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from about.models import About
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -8,6 +10,13 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'content', 'updated_on')
+    search_fields = ['title', 'content']
+    list_filter = ('updated_on',)
     summernote_fields = ('content',)
 
 # Register your models here.
